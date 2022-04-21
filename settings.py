07fb0999f8ad570496d071fccfafb2ce6a89d4a5
shay <shayventura12@gmail.com>
2022-04-21 18:23:01 +0300

    first commit

---
+++ settings.py
@@ -0,0 +1,56 @@
+class Settings:
+
+    def __init__(self):
+
+       
+
+    
+        
+        #screen settings
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)
+        
+        #ship settings
+        self.ship_limit = 3
+
+        #bullet setting
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60, 60, 60)
+        self.bullet_allowed = 3
+        
+        #alieb settings
+        self.fleet_drop_Speed = 1
+        
+        #how quickly the game speeds up
+        self.speedup_scale = 1.1
+        #how quickly the point valus increase
+        self.score_scale = 1.5
+        
+        self.initialize_dynamic_settings()
+        
+    def initialize_dynamic_settings(self):
+        self.ship_speed = 1.5
+        self.bullet_speed = 3.0
+        self.alien_speed = 0.5
+        
+        self.fleet_direction = -1
+
+        
+        
+        #scoring
+        self.alien_points = 50 
+
+    
+
+    def increase_speed(self):
+        self.ship_speed *= self.speedup_scale
+        self.bullet_speed *= self.speedup_scale
+        self.alien_speed *= self.speedup_scale
+
+        self.alien_points = int(self.alien_points * self.score_scale)
+
+
+
+
---
+++ ship.py
@@ -0,0 +1,37 @@
+import pygame
+
+class Ship:
+
+    def __init__(self, ai_game):
+        self.screen = ai_game.screen
+        self.setting = ai_game.settings
+        self.screen_rect = ai_game.screen.get_rect()
+
+        self.image = pygame.image.load('images/space_ship.bmp')
+        self.rect = self.image.get_rect()
+        
+        self.rect.midbottom = self.screen_rect.midbottom
+
+        self.x = float(self.rect.x)
+
+        #movement flaf
+        self.moving_right = False
+        self.moving_left = False
+
+
+    def update(self):
+        """update the shops posision based on movement flag"""
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.x += self.setting.ship_speed
+
+        if self.moving_left and self.rect.left > 0:
+            self.x -= self.setting.ship_speed
+
+        self.rect.x = self.x
+    
+    def blitme(self):
+       self.screen.blit(self.image,self.rect)
+
+    def center_ship(self):
+        self.rect.midbottom = self.screen_rect.midbottom
+        self.x = float(self.rect.x)
